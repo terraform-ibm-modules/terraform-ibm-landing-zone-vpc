cd8dea7a75a2e9b85ac48e7a3afc7140d954c42e
Andrej Kocbek <akocbek@ie.ibm.com>
2022-06-15 13:26:03 +0100

    feat: add tflint support

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -18,7 +18,7 @@ data "ibm_resource_group" "existing_resource_group" {
 #############################################################################
 
 module "deploy_vpc" {
-  source               = "../"
+  source               = "../../"
   resource_group_id    = var.resource_group != null ? data.ibm_resource_group.existing_resource_group[0].id : ibm_resource_group.resource_group[0].id
   region               = var.region
   prefix               = var.prefix
