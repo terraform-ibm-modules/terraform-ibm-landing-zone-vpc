f7968b6c9b40ebc63a939a913603dee19afa33bf
Adam Geiger <ageiger@us.ibm.com>
2022-11-15 11:31:52 -0500

    fix: remove acl and sg validation rule

--- variables.tf
+++ variables.tf
@@ -160,24 +160,6 @@ variable "network_acls" {
   ]
 
   validation {
-    error_message = "ACL rules can only have one of `icmp`, `udp`, or `tcp`."
-    condition = length(distinct(
-      # Get flat list of results
-      flatten([
-        # Check through rules
-        for rule in flatten([var.network_acls[*].rules]) :
-        # Return true if there is more than one of `icmp`, `udp`, or `tcp`
-        true if length(
-          [
-            for type in ["tcp", "udp", "icmp"] :
-            true if rule[type] != null
-          ]
-        ) > 1
-      ])
-    )) == 0 # Checks for length. If all fields all correct, array will be empty
-  }
-
-  validation {
     error_message = "ACL rule actions can only be `allow` or `deny`."
     condition = length(distinct(
       flatten([
@@ -343,24 +325,6 @@ variable "security_group_rules" {
       )
     })
   )
-
-  validation {
-    error_message = "Security group rules can only have one of `icmp`, `udp`, or `tcp`."
-    condition = (var.security_group_rules == null || length(var.security_group_rules) == 0) ? true : length(distinct(
-      # Get flat list of results
-      flatten([
-        # Check through rules
-        for rule in var.security_group_rules :
-        # Return true if there is more than one of `icmp`, `udp`, or `tcp`
-        true if length(
-          [
-            for type in ["tcp", "udp", "icmp"] :
-            true if rule[type] != null
-          ]
-        ) > 1
-      ])
-    )) == 0 # Checks for length. If all fields all correct, array will be empty
-  }
 
   validation {
     error_message = "Security group rule direction can only be `inbound` or `outbound`."
