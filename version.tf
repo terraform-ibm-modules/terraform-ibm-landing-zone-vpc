9dac9bba2e156ff45809f10022a97e2a81ef7f34
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-12-01 18:20:46 +0000

    fix(deps): update terraform ibm to v1.48.0 (#199)
    * fix(deps): update terraform ibm to v1.48.0
    
    * docs: committing files modified by the hook
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- version.tf
+++ version.tf
@@ -5,7 +5,7 @@ terraform {
     # Use "greater than or equal to" range in modules
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.45.0"
+      version = ">= 1.48.0"
     }
   }
 }
