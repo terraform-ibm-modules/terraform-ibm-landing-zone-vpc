9910e4b534f13dcc67e0f6bf79b7fbbf14fcebed
Aashiq-Jacob <aashiq.jacob@ibm.com>
2023-03-24 22:00:19 +0530

    fix: update the resource group name

--- examples/landing_zone/main.tf
+++ examples/landing_zone/main.tf
@@ -12,7 +12,7 @@ module "workload_resource_group" {
 module "management_resource_group" {
   source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-resource-group.git?ref=v1.0.5"
   # if an existing resource group is not set (null) create a new one using prefix
-  resource_group_name          = var.management_resource_group == null ? "${var.prefix}-workload-rg" : null
+  resource_group_name          = var.management_resource_group == null ? "${var.prefix}-management-rg" : null
   existing_resource_group_name = var.management_resource_group
 }
 
