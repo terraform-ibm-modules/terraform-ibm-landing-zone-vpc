c629f1475e1965fb9274db0cf98a7c1d873c30c8
Aashiq-J <aashiq.jacob@ibm.com>
2023-04-01 13:11:58 +0530

    fix: bug in pr_test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -14,10 +14,10 @@ const resourceGroup = "geretain-test-resources"
 // The ACL ignores can be removed once we merge this PR (https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc/pull/471)
 var ignoreUpdates = []string{"module.slz_vpc.ibm_is_network_acl.network_acl[\"vpc-acl\"]"}
 
-func setupOptions(t *testing.T, prefix string) *testhelper.TestOptions {
+func setupOptions(t *testing.T, prefix string, terraformDir string) *testhelper.TestOptions {
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:       t,
-		TerraformDir:  defaultExampleTerraformDir,
+		TerraformDir:  terraformDir,
 		Prefix:        prefix,
 		ResourceGroup: resourceGroup,
 		IgnoreUpdates: testhelper.Exemptions{
