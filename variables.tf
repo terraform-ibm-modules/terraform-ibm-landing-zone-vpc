06736e8084f2d5d0d9bd100d8a67da2b76dd3236
Aashiq-J <aashiq.jacob@ibm.com>
2023-05-24 20:29:22 +0530

    test: update variable description

--- variables.tf
+++ variables.tf
@@ -48,7 +48,7 @@ variable "access_tags" {
 ##############################################################################
 
 variable "network_cidrs" {
-  description = "Network CIDR for the VPC. This is used to manage network ACL rules for cluster provisioning."
+  description = "List of Network CIDRs for the VPC. This is used to manage network ACL rules for cluster provisioning."
   type        = list(string)
   default     = ["10.0.0.0/8"]
 }
