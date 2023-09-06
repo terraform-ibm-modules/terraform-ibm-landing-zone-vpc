c7033e02164667c711d5ffc81c8e562ed6678bf8
kierra.searle@ibm.com <kierra.searle@ibm.com>
2023-09-06 11:27:11 -0400

    fix: removed locals

--- examples/no-prefix/main.tf
+++ examples/no-prefix/main.tf
@@ -15,37 +15,20 @@ data "ibm_resource_group" "existing_resource_group" {
 }
 
 #############################################################################
-# Locals
-#############################################################################
-locals {
-  access_tags                            = []
-  enable_vpc_flow_logs                   = true
-  cos_plan                               = "standard"
-  cos_location                           = "global"
-  create_authorization_policy_vpc_to_cos = true
-  network_cidrs                          = ["10.0.0.0/8", "164.0.0.0/8"]
-  address_prefixes = {
-    zone-1 = ["10.10.10.0/24"]
-    zone-2 = ["10.20.10.0/24"]
-    zone-3 = ["10.30.10.0/24"]
-  }
-}
-
-#############################################################################
 # Provision cloud object storage and bucket
 #############################################################################
 
 resource "ibm_resource_instance" "cos_instance" {
-  count             = local.enable_vpc_flow_logs ? 1 : 0
+  count             = 1
   name              = "${var.prefix}-vpc-logs-cos"
   resource_group_id = var.resource_group != null ? data.ibm_resource_group.existing_resource_group[0].id : ibm_resource_group.resource_group[0].id
   service           = "cloud-object-storage"
-  plan              = local.cos_plan
-  location          = local.cos_location
+  plan              = "standard"
+  location          = "global"
 }
 
 resource "ibm_cos_bucket" "cos_bucket" {
-  count                = local.enable_vpc_flow_logs ? 1 : 0
+  count                = 1
   bucket_name          = "${var.prefix}-vpc-logs-cos-bucket"
   resource_instance_id = ibm_resource_instance.cos_instance[0].id
   region_location      = var.region
@@ -63,11 +46,15 @@ module "slz_vpc" {
   name                                   = var.name
   prefix                                 = null
   tags                                   = var.resource_tags
-  access_tags                            = local.access_tags
-  enable_vpc_flow_logs                   = local.enable_vpc_flow_logs
-  create_authorization_policy_vpc_to_cos = local.create_authorization_policy_vpc_to_cos
+  access_tags                            = []
+  enable_vpc_flow_logs                   = true
+  create_authorization_policy_vpc_to_cos = true
   existing_cos_instance_guid             = ibm_resource_instance.cos_instance[0].guid
   existing_storage_bucket_name           = ibm_cos_bucket.cos_bucket[0].bucket_name
-  address_prefixes                       = local.address_prefixes
-  network_cidrs                          = local.network_cidrs
+  address_prefixes = {
+    zone-1 = ["10.10.10.0/24"]
+    zone-2 = ["10.20.10.0/24"]
+    zone-3 = ["10.30.10.0/24"]
+  }
+  network_cidrs = ["10.0.0.0/8", "164.0.0.0/8"]
 }
