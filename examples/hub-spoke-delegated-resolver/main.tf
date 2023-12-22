dc40d987dcfc4a577d6c1f6940d2139c9b5a914d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-12-22 22:24:13 +0000

    chore(deps): update terraform-module

--- examples/hub-spoke-delegated-resolver/main.tf
+++ examples/hub-spoke-delegated-resolver/main.tf
@@ -96,7 +96,7 @@ module "spoke_vpc" {
 
 module "tg_gateway_connection" {
   source                    = "terraform-ibm-modules/transit-gateway/ibm"
-  version                   = "2.3.1"
+  version                   = "2.4.0"
   transit_gateway_name      = "${var.prefix}-tg"
   region                    = var.region
   global_routing            = false
