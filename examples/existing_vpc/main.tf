6b81bcbd14712d1d86fdaa4c2bd3f5603a482227
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-06-15 09:43:05 +0000

    chore(deps): update terraform-module

--- examples/existing_vpc/main.tf
+++ examples/existing_vpc/main.tf
@@ -5,7 +5,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.5"
+  version = "1.1.6"
   # if an existing resource group is not set (null) create a new one using prefix
   existing_resource_group_name = var.existing_resource_group_name
 }
