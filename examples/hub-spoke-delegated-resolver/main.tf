e9db106eeb3f90bce517a0b882a483d3d8298e39
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-15 15:07:57 -0400

    chore(deps): update terraform-module (#801)

--- examples/hub-spoke-delegated-resolver/main.tf
+++ examples/hub-spoke-delegated-resolver/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.5"
+  version = "1.1.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -114,7 +114,7 @@ module "spoke_vpc" {
 
 module "tg_gateway_connection" {
   source                    = "terraform-ibm-modules/transit-gateway/ibm"
-  version                   = "2.4.2"
+  version                   = "2.4.3"
   transit_gateway_name      = "${var.prefix}-tg"
   region                    = var.region
   global_routing            = false
