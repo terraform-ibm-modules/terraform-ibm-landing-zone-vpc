af3f8f1b53922153c9d08610ad8dad94de6fa1fa
kierra.searle@ibm.com <kierra.searle@ibm.com>
2024-08-29 12:01:00 -0400

    feat: added new example

---
+++ examples/specific-zone-only/README.md
@@ -0,0 +1,8 @@
+# Specific Zone Only Example
+
+A simple example to provision a Secure Landing Zone (SLZ) Virtual Private Cloud (VPC) in a specific zone other than Zone 1. Also, shows how to use public gateways with a specific zone. In this example Zone 2 is used.
+
+The following resources are provisioned by this example:
+
+* A new resource group, if an existing one is not passed in.
+* An IBM Virtual Private Cloud (VPC) with a publicly exposed subnet.
---
+++ examples/specific-zone-only/main.tf
@@ -0,0 +1,40 @@
+##############################################################################
+# Resource Group
+##############################################################################
+
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.1.5"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+#############################################################################
+# Provision VPC
+#############################################################################
+
+module "slz_vpc" {
+  source            = "../../"
+  resource_group_id = module.resource_group.resource_group_id
+  region            = var.region
+  name              = var.name
+  prefix            = var.prefix
+  tags              = var.resource_tags
+  subnets = {
+    zone-1 = []
+    zone-2 = [
+      {
+        name           = "subnet-a"
+        cidr           = "10.10.10.0/24"
+        public_gateway = true
+        acl_name       = "vpc-acl"
+      }
+    ]
+  }
+  use_public_gateways = {
+    zone-1 = false
+    zone-2 = true
+    zone-3 = false
+  }
+}
