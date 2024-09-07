e56dd14bf1558ca5a630fe83f368d9c85591afda
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-09-07 14:48:19 +0000

    chore(deps): update terraform terraform-ibm-modules/cos/ibm to v8.11.5

--- examples/landing_zone/main.tf
+++ examples/landing_zone/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "cos_bucket" {
   count                  = var.enable_vpc_flow_logs ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.11.3"
+  version                = "8.11.5"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cross_region_location  = null
