7eca40972abf8cdb457d110525e55b6fbe5e9103
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-01 22:31:50 +0000

    chore(deps): update terraform-module

--- examples/landing_zone/main.tf
+++ examples/landing_zone/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "cos_bucket" {
   count                  = var.enable_vpc_flow_logs ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.16.4"
+  version                = "8.19.5"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cross_region_location  = null
@@ -63,7 +63,7 @@ module "management_vpc" {
 
 module "tg_gateway_connection" {
   source                    = "terraform-ibm-modules/transit-gateway/ibm"
-  version                   = "2.4.3"
+  version                   = "2.5.0"
   transit_gateway_name      = "${var.prefix}-tg"
   region                    = var.region
   global_routing            = false
