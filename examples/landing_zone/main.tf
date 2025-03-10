86e2856262eccc5faf98bcd79749e6350291cf5a
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-10 12:04:02 +0000

    chore(deps): update terraform-module (#911)
    Co-authored-by: Shikha Maheshwari <maheshwari.shikha@gmail.com>

--- examples/landing_zone/main.tf
+++ examples/landing_zone/main.tf
@@ -17,7 +17,7 @@ module "resource_group" {
 module "cos_bucket" {
   count                  = var.enable_vpc_flow_logs ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "8.16.4"
+  version                = "8.19.5"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cross_region_location  = null
