b58a2d468c48e319be4f98f342aa4d9a758246a4
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-03-18 09:25:06 +0530

    add private path output

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -1,5 +1,5 @@
 locals {
-  prefix = (var.prefix != null && trimspace(var.prefix) != "" ? var.prefix : "")
+  prefix = var.prefix != null ? trimspace(var.prefix) != "" ? var.prefix : "" : ""
 }
 
 ##############################################################################
