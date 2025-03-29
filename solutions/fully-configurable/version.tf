622b5df0798d24aede9f2d15376eb70cd221ba33
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-03-29 13:44:15 +0000

    fix(deps): update terraform ibm to latest for deployable architectures

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.76.1"
+      version = "1.76.3"
     }
     time = {
       source  = "hashicorp/time"
