1f28e9ddfc99781bb3ccd0614b8ef2927a0999eb
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-05-06 17:07:30 +0530

    fix zones

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -107,13 +107,14 @@ variable "subnets" {
         acl_name       = "vpc-acl"
         no_addr_prefix = false
       }
-    ]
+    ],
     zone-2 = [
       {
         name           = "subnet-b"
         cidr           = "10.20.10.0/24"
         public_gateway = false
         acl_name       = "vpc-acl"
+        no_addr_prefix = false
       }
     ],
     zone-3 = [
@@ -122,6 +123,7 @@ variable "subnets" {
         cidr           = "10.30.10.0/24"
         public_gateway = false
         acl_name       = "vpc-acl"
+        no_addr_prefix = false
       }
     ]
   }
