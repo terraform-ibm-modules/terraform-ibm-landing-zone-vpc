d835fb88d6f12eabbbab237190479fc91ec9459a
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-05-13 11:16:51 +0100

    fix(deps): update DA provider version to 1.78.2

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.78.1"
+      version = "1.78.2"
     }
     time = {
       source  = "hashicorp/time"
