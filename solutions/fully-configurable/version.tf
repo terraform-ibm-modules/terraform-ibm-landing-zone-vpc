917ddc19c0cabdccf0f4d7360bcc74881137ae10
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2025-06-10 00:35:47 +0530

    update provider version

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.78.4"
+      version = "1.79.0"
     }
     time = {
       source  = "hashicorp/time"
