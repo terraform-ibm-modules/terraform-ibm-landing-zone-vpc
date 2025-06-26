29a907da5c15835344621b0e610d4e17390df7d4
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2025-06-26 11:25:02 +0530

    feat: improve DA user experience

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -27,9 +27,15 @@ variable "existing_resource_group_name" {
 
 variable "prefix" {
   type        = string
-  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-vpc. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix)."
+  nullable    = true
+  description = "The prefix to be added to all resources created by this solution. To skip using a prefix, set this value to null or an empty string. The prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It should not exceed 16 characters, must not end with a hyphen('-'), and can not contain consecutive hyphens ('--'). Example: prod-0205-vpc. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/prefix.md)."
 
   validation {
+    # - null and empty string is allowed
+    # - Must not contain consecutive hyphens (--): length(regexall("--", var.prefix)) == 0
+    # - Starts with a lowercase letter: [a-z]
+    # - Contains only lowercase letters (a–z), digits (0–9), and hyphens (-)
+    # - Must not end with a hyphen (-): [a-z0-9]
     condition = (var.prefix == null || var.prefix == "" ? true :
       alltrue([
         can(regex("^[a-z][-a-z0-9]*[a-z0-9]$", var.prefix)),
@@ -38,7 +44,9 @@ variable "prefix" {
     )
     error_message = "Prefix must begin with a lowercase letter and may contain only lowercase letters, digits, and hyphens '-'. It must not end with a hyphen('-'), and cannot contain consecutive hyphens ('--')."
   }
+
   validation {
+    # must not exceed 16 characters in length
     condition     = length(var.prefix) <= 16
     error_message = "Prefix must not exceed 16 characters."
   }
@@ -51,9 +59,9 @@ variable "vpc_name" {
 }
 
 variable "region" {
-  default     = "us-south"
-  description = "Region to deploy the VPC."
   type        = string
+  description = "The region to provision all resources in. [Learn more](https://terraform-ibm-modules.github.io/documentation/#/region) about how to select different regions for different services."
+  default     = "us-south"
 }
 
 variable "resource_tags" {
