67d21beda8e697066f0bccadb8542ebc133c249f
Vipin Kumar <kumarvipin88073@gmail.com>
2025-07-30 23:36:25 +0530

    docs: vpe security group ids

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -713,6 +713,12 @@ variable "vpe_gateway_service_endpoints" {
 }
 
 variable "vpe_gateway_security_group_ids" {
+
+  # Currently unused — the DA doesn't create any custom security groups.
+  # The default security group (automatically created with the VPC) is attached to the VPE gateway since no other security groups are present.
+  # May be useful in the future when DA supports using an existing VPC with custom security groups.
+
+
   description = "List of security group ids to attach to each endpoint gateway."
   type        = list(string)
   default     = null # Let this default value be null instead of []. Provider issue - https://github.com/IBM-Cloud/terraform-provider-ibm/issues/4546
