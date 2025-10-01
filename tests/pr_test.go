c6322ce90734b829adba21bd233d75c40e259498
kierra.searle@ibm.com <kierra.searle@ibm.com>
2025-10-01 10:58:03 -0400

    refactor: terraform vars in noprefix test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -91,11 +91,7 @@ func TestRunNoPrefixExample(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptions(t, "no-prefix-lz", noprefixExampleTerraformDir)
-	options.TerraformVars = map[string]interface{}{
-		"prefix":   options.Prefix,
-		"region":   options.Region,
-		"vpc_name": fmt.Sprintf("%s-vpc", options.Prefix),
-	}
+	options.TerraformVars["vpc_name"] = fmt.Sprintf("%s-vpc", options.Prefix)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
