62792d2bbb905506db59c80aacdbf38379510b9e
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-17 16:56:22 +0000

    fix(deps): update dependencies (#1108)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -230,7 +230,7 @@ module "vpc" {
 
 module "vpe_gateway" {
   source               = "terraform-ibm-modules/vpe-gateway/ibm"
-  version              = "5.0.0"
+  version              = "5.0.1"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   prefix               = local.prefix
