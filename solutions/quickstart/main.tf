8302785872acb33c36d3659ca803c9424d154b25
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-31 01:55:10 +0000

    fix(deps): update dependencies (#1120)

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -20,7 +20,7 @@ module "resource_group" {
 module "cos" {
   count                  = var.enable_vpc_flow_logs ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.9.6"
+  version                = "10.9.7"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cos_instance_name      = "${var.prefix}-cos"
