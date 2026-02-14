77d790300ff73fffdec49601c95f998acc70dcb9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-02-14 06:21:57 +0000

    fix(deps): update dependencies

--- solutions/quickstart/version.tf
+++ solutions/quickstart/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.87.3"
+      version = "1.88.2"
     }
   }
 }
