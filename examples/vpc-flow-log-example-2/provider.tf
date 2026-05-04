02e00dc9d17d0582b37c66f4ca8f0c504d419555
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-05-04 19:42:48 +0530

    remove testing example

--- examples/vpc-flow-log-example-2/provider.tf
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# Terraform Provider Configuration
-##############################################################################
-
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-}
