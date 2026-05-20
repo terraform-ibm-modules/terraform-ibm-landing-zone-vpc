66d946aadf4cbd11b2e5de931879c9a26218868b
Mukul Palit <mukul.palit@ibm.com>
2026-05-20 13:27:30 +0530

    fix(deps): update dependencies <br>- deprecated terratest functions and add context (#1180)
    * fix(deps): update dependencies
    
    * fix(deps): deprecated terratest functions and add context
    
    * Update main.tf
    
    * Update main.tf
    
    ---------
    
    Co-authored-by: Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
    Co-authored-by: Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>

--- examples/landing_zone/main.tf
+++ examples/landing_zone/main.tf
@@ -17,12 +17,12 @@ module "resource_group" {
 module "cos_bucket" {
   count                  = var.enable_vpc_flow_logs ? 1 : 0
   source                 = "terraform-ibm-modules/cos/ibm"
-  version                = "10.15.1"
+  version                = "10.16.0"
   resource_group_id      = module.resource_group.resource_group_id
   region                 = var.region
   cross_region_location  = null
   cos_instance_name      = "${var.prefix}-vpc-logs-cos"
-  cos_tags               = var.resource_tags
+  resource_tags          = var.resource_tags
   bucket_name            = "${var.prefix}-vpc-logs-cos-bucket"
   kms_encryption_enabled = false
 }
