f54d1b522696e898f892a2304f31ba37e9dd70b5
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-08-06 12:06:04 +0530

    update provider version

--- examples/specific-zone-only/version.tf
+++ examples/specific-zone-only/version.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.9.0"
   required_providers {
-    # Now using the stable 2.5.0 release which resolves the ACL rule destroy/recreate issue on inline updates.
+    # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = ">= 2.5.0"
